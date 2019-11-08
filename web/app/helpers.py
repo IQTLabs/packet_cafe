de2b6b1590f80ebba270b730ec5776fae75badb9
cglewis <clewis@iqt.org>
2019-11-08 09:36:54 -0800

    closes #93

--- web/app/helpers.py
+++ web/app/helpers.py
@@ -2,6 +2,6 @@ import json
 
 def load_tools():
     tools = {}
-    with open('workers.json') as json_file:
+    with open('/definitions/workers.json') as json_file:
         tools = json.load(json_file)
     return tools
