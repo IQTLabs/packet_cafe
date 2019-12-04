0b280026dbbaac0f375629b81b56081c9b5ab871
ryan <rashley@iqt.org>
2019-12-04 10:03:16 -0500

    Stubbed out version of redux integration

---
+++ ui/src/domain/root-reducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import data from "./data";
+// import controls from "./controls";
+// import error from "./error";
+
+export default combineReducers({
+  data,
+  //controls,
+  //error,
+});
