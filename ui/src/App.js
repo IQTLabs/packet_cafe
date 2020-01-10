02be9210fbccd1c4be5fae443ad298a6cf15a9d1
Hope Tambala <hopetambala@outlook.com>
2020-01-10 01:41:45 -0500

    chore: remove unused imports

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { Button, Grid } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
 
 import { fetchResults } from 'epics/fetch-results-epic'
 
-import logo from './logo.svg';
 import './App.css';
 import Upload from 'components/upload/Upload';
-import Navbar from 'components/Header';
-import TabsComponent from 'components/tabs/Tabs';
+import Navbar from 'components/Navbar';
 import Table from 'components/table/Table.js';
 
 const uuidv4 = require('uuid/v4');
@@ -19,9 +17,6 @@ class App extends React.Component {
 
   fetchResults = () => {
       console.log("Peasant Burnination initiated...");
-      //this.props.showBusy(true);
-      //this.props.setIsFetching({owner: this.props.uuid, isFetching: true});
-      //const authHeader = buildAuthHeader(username, password, token);
       this.props.fetchResults({ 'sessionId': SESSION_ID });
       console.log("Peasant Burnination complete!");
   }
@@ -31,22 +26,22 @@ class App extends React.Component {
       <>
         <Navbar/>
         <Grid textAlign='center' style={{ height: '100vh' }} divided='vertically'>
-                  <Grid.Row columns={1}>
-                  <Grid.Column style={{ maxWidth: 240 }}>
-                    <Upload sessionId={SESSION_ID}/>
-                  </Grid.Column>
-                  </Grid.Row>
-                  <Grid.Row columns={1}>
-                      <Grid.Column>
-                          <div>
-                              <button onClick={this.fetchResults}>
-                                  Burninate Peasants
-                              </button>
-                          </div>
-                          <Table sessionId={SESSION_ID}/>
-                      </Grid.Column>
-                  </Grid.Row>
-              </Grid>
+          <Grid.Row columns={1}>
+            <Grid.Column style={{ maxWidth: 240 }}>
+              <Upload sessionId={SESSION_ID}/>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row columns={1}>
+            <Grid.Column>
+              <div>
+                <button onClick={this.fetchResults}>
+                  Burninate Peasants
+                </button>
+              </div>
+              <Table sessionId={SESSION_ID}/>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </>
     );
   }
