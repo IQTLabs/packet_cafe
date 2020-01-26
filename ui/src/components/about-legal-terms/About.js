e6875f595421fad4bd7e3c0fc51482464553e523
Hope Tambala <hopetambala@outlook.com>
2020-01-26 00:15:33 -0500

    chore: remove unused imports

--- ui/src/components/about-legal-terms/About.js
+++ ui/src/components/about-legal-terms/About.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Header, Icon, Modal } from 'semantic-ui-react';
+import { Header, Modal } from 'semantic-ui-react';
 
 export default class AboutModal extends Component {
   state = { modalOpen: false }
