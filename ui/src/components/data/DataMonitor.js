0189868893b34e54b974d0c53ac9946cf34f5ae7
ryan <rashley@iqt.org>
2020-04-20 15:39:20 -0400

    added fall through for null case

--- ui/src/components/data/DataMonitor.js
+++ ui/src/components/data/DataMonitor.js
@@ -7,7 +7,7 @@ import { fetchToolResults } from 'epics/fetch-tool-results-epic'
 
 const DataMonitor = (props) => {
     const dispatch = useDispatch();
-    const toolResults = useSelector(state => state.data.toolResults);
+    const toolResults = useSelector(state => state.data.toolResults) || {};
 
     const getCompleted = (statuses, fileResults) => {
         var completed = [];
@@ -35,7 +35,8 @@ const DataMonitor = (props) => {
     useEffect(() => {
         if(props.files && props.statuses)
         for(const file of props.files){
-            const completed = getCompleted(props.statuses[file.id], toolResults[file.id]);
+            const fileResults = toolResults[file.id] || {};
+            const completed = getCompleted(props.statuses[file.id], fileResults);
             for(const tool of completed){
                 const payload = {
                     'sessionId': props.sessionId, 
