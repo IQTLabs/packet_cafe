d7e4e6397439f6888e36ebc6d7b241b52fa65746
ryan <rashley@iqt.org>
2020-07-17 16:57:30 -0400

    merge struggles

--- ui/src/domain/data_wrangling.js
+++ ui/src/domain/data_wrangling.js
@@ -6,14 +6,22 @@ const defaultState = {
   heatmap: {
     ipResults:null,
     portResults:null
-  }
+  },
+  packetstats:null
 };
 
 const removeNumbersLessThan = (num, obj) => {
+  var items = [];
   for (var key in obj) {                    // for each key in the object
     if(!isNaN(obj[key]) && obj[key] < num)  // if the value of that key is not a NaN (is a number) and if that number is greater than num
-      delete obj[key];                      // then delete the key-value from the object
+      delete obj[key];  
+    else
+      var item = {};
+      item[key] = obj[key];
+      items.push(item);                    // then delete the key-value from the object
   }
+
+  return items;
 }
 
 const configureHeatmapData = (payload) => {
@@ -26,7 +34,7 @@ const configureHeatmapData = (payload) => {
   });
 
   const uniquemappedfirstKey = [...new Set(mappedfirstKey)];
-  
+
   /**
    * Count of Source IP Keys by Destination IP Keys
   */
@@ -38,34 +46,101 @@ const configureHeatmapData = (payload) => {
       return v.length ; 
     })
     .object(data);
-    
+
   var counts = Object.entries(dataByFirstKeyBySecondKey)
     .map(([firstKey, secondKeys]) => {
-      return ({ firstKey, secondKeys });
+      return ({ firstKey, ...secondKeys });
     });
+  //console.log("counts: %o", counts)
 
   var secondKeys = counts.map((dat)=>{
     return dat.secondKeys
   });
 
-  var combinedData = counts.map((item, i) => Object.assign({}, item, secondKeys[i]));
+  //console.log("secondKeys: %o", secondKeys);
 
-  combinedData.map((dat) =>{
-    Object.entries(dat).map(() => {
-      delete dat.secondKeys
-      return removeNumbersLessThan(1,dat)
-    })
-  })
+  var combinedData = counts.map((item, i) => {
+    return Object.assign({}, item, secondKeys[i])
+  });
 
+  //console.log("combined data pre removal: %o", combinedData);
+  // combinedData.map((dat) =>{
+  //   Object.entries(dat).map(() => {
+  //     delete dat.firstKey
+  //     delete dat.secondKeys
+  //     const gto = removeNumbersLessThan(1,dat) || [];
+  //     return gto;
+  //   })
+  // })
+  //console.log("combined data: %o", combinedData);
   return {
-    transformedData: combinedData,
-    uniqueListOfKeys: uniquemappedfirstKey,
+    data: combinedData,
+    keys: uniquemappedfirstKey,
     dataType: payload.type
   }
 }
 
+const configurePacketStatistics = (payload) => {
+  var endpointsArray = []
+
+  for (var key in payload){
+    if (key.includes('Endpoints')){
+      var obj = {};
+      obj.data = payload[key];
+      obj.Type = key;
+      endpointsArray.push(obj);
+    }
+  }
+
+
+  /**
+   * Statistics for Each Endpoint
+  */
+
+  const fullArray = endpointsArray.map((obj)=>{
+    obj['Maximum Packet Size'] = d3.max(obj.data, function(d) { return d['Packets']; });
+    obj['Minimum Packet Size'] = d3.min(obj.data, function(d) { return d['Packets']; });
+    obj['Average Packet Size'] = d3.mean(obj.data, function(d) { return d['Packets']; });
+    obj['Sum Packet Size'] = d3.sum(obj.data, function(d) { return d['Packets']; });
+
+    obj['Maximum Bytes Size'] = d3.max(obj.data, function(d) { return d['Bytes']; });
+    obj['Minimum Bytes Size'] = d3.min(obj.data, function(d) { return d['Bytes']; });
+    obj['Average Bytes Size'] = d3.mean(obj.data, function(d) { return d['Bytes']; });
+    obj['Sum Byte Size'] = d3.sum(obj.data, function(d) { return d['Bytes']; });
+
+    return obj;
+  });
+
+
+
+  const filteredArray = fullArray.filter((obj)=>{
+    //Remove undefined
+    if (obj['data'].length > 1){
+      return obj
+    } 
+  });
+
+  const packetsum = d3.sum(filteredArray, function(d) { return d['Sum Packet Size']; });
+  const bytesum = d3.sum(filteredArray, function(d) { return d['Sum Byte Size']; });
+
+  /**
+   * Percentages of Packet Size and Byte Size for each endpoint
+  */
+  var filteredArrayWithPercentages = filteredArray.map((item)=>{
+    item["Percentage of Total Packet Size"] = item['Sum Packet Size'] / packetsum
+    item["Percentage of Total Byte Size"] = item['Sum Byte Size'] / bytesum
+
+    return item
+  })
+
+  // console.log(filteredArrayWithPercentages)
+
+  return filteredArrayWithPercentages
+}
+
 // ACTIONS
 const setHeatmapData = createAction("SET_HEATMAP_DATA");
+const setPacketStatisticsData = createAction("SET_PACKETSTATS_DATA");
 
 // REDUCERS
 const reducer = handleActions(
@@ -74,14 +149,12 @@ const reducer = handleActions(
       const transformedData = configureHeatmapData(payload);
       if(payload.type == "ip"){
         state.heatmap.ipResults = transformedData;
-        console.log(transformedData);
         return{
           ...state
         }
       }
       else if(payload.type == "port"){
         state.heatmap.portResults = transformedData;
-        console.log(transformedData);
         return{
           ...state
         }
@@ -90,13 +163,19 @@ const reducer = handleActions(
         ...state
       };
     },
+    [setPacketStatisticsData]: (state, { payload }) => {
+      const transformedData = configurePacketStatistics(payload);
+      state.packetstats = transformedData;
+      return{
+        ...state
+      }
+    },
   },
   defaultState
 );
 
 const getDataWranglingState = (state) => state.data_wrangling;
 
-export { setHeatmapData, getDataWranglingState}
+export { setHeatmapData, setPacketStatisticsData, getDataWranglingState, configureHeatmapData }
 
 export default reducer;
-
