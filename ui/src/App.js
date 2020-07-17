c951690aa68472f84fbbf212dd2265c60a47a729
ryan <rashley@iqt.org>
2020-07-17 16:29:29 -0400

    correct merge conflicts

--- ui/src/App.js
+++ ui/src/App.js
@@ -9,21 +9,58 @@ import { Grid } from 'semantic-ui-react';
 import { startFetchResults, stopFetchResults } from "epics/auto-fetch-results-epic"
 import { fetchResults } from 'epics/fetch-results-epic'
 import { fetchToolStatus } from 'epics/fetch-status-epic'
-<<<<<<< HEAD
-import { setHeatmapData, getDataWranglingState } from 'domain/data_wrangling';
-=======
 import { fetchTools } from 'epics/fetch-tools-epic'
->>>>>>> cf11b1644903bcb359730349b002cc06679b4c7e
-import { setSessionId, getResults, getToolStatuses } from 'domain/data';
+import { setPacketStatisticsData,  getDataWranglingState, configureHeatmapData } from 'domain/data_wrangling';
+import { setSessionId, getResults, getToolStatuses, getToolResults } from 'domain/data';
 
 import './App.css';
 import Upload from 'components/upload/Upload';
 import Navbar from 'components/Navbar';
 import DataMonitor from 'components/data/DataMonitor';
-import Table from 'components/table/Table.js';
-import Heatmap from 'components/heatmap/Heatmap.js';
+import Table from 'components/table/Table';
+import Heatmap from 'components/heatmap/Heatmap';
+import PcapCard from 'components/pcapstats/PcapCard';
+
+import pcapStatsData from 'components/pcapstats/data.json';
 
 const COOKIE_NAME = 'sessionID'
+
+const formatHeatmapData = (files, results) => {
+    const selectedFile = files.length > 0 ? files[0].id : "";
+    const statusArray = !results[selectedFile] ? [] : Object.keys(results[selectedFile]).map(key => ({
+      tool: String(key), 
+      id: selectedFile,
+      results: results[selectedFile][key]
+    })) 
+
+    const mercury = statusArray.filter((data)=>{
+        return data.tool === "mercury"
+    })
+
+    const mercuryResults = mercury.length > 0 && mercury[0].results.length > 0 ? mercury[0].results[0] : []; 
+
+    var ipData = {
+      type:"ip",
+      // data:jsonData[0],
+      data:mercuryResults,
+      firstKey:"dst_ip",
+      secondKey:"src_ip"
+    }
+
+    ipData = configureHeatmapData(ipData)
+
+    var portData = {
+      type:"port",
+      // data:jsonData[0],
+      data: mercuryResults,
+      firstKey:"dst_port",
+      secondKey:"src_port"
+    }
+
+    portData = configureHeatmapData(portData)
+
+    return { 'ipResults': ipData, 'portResults': portData }
+  }
 
 class App extends React.Component {
   static propTypes = {
@@ -44,110 +81,61 @@ class App extends React.Component {
     }
     this.props.setSessionId(sessionId);
     this.state = {
-      ipResults: [],
-      portResults:[],
+      ipResults:null,
+      portResults: null,
+      packetStats:null,
       sessionId: sessionId
     };
   }
 
-<<<<<<< HEAD
+  componentDidMount(){
+    this.fetchStatsData()
+  }
+
   static getDerivedStateFromProps(props, state) {
     // Any time the current user changes,
     // Reset any parts of state that are tied to that user.
     // In this simple example, that's just the email.
-    const { ipResults, portResults } = props.vizData.heatmap;
-    if (props.ipResults !== state.ipResults) {
+    const { vizData } = props;
+    const hmData = formatHeatmapData(props.files, props.results);
+
+    if (hmData !== state.vizData) {
       return {
-        ipResults: ipResults,
-        portResults: portResults
+        ipResults: hmData.ipResults,
+        portResults: hmData.portResults,
+        packetStats: vizData.packetstats
       };
     }
-    return null;
   }
 
-  fetchResults = () => {
-      console.log("Peasant Burnination initiated...");
-      this.props.fetchResults({ 'sessionId': this.state.sessionId });
-      console.log("Peasant Burnination complete!");
-      this.fetchHeatmapData();
-=======
   componentWillMount() {
     this.props.fetchTools();
->>>>>>> cf11b1644903bcb359730349b002cc06679b4c7e
   }
 
   componentWillUnmount() {
     this.props.stopFetchResults();
   }
 
-<<<<<<< HEAD
-  fetchHeatmapData = async () => {
-    const { setHeatmapData, rows, statuses } = this.props;
-    const { sessionId } = this.state;
-
-    const statusArray = Object.keys(statuses).map(key => ({
-      tool: String(key), 
-      id: rows[0].id,
-      ...statuses[key]
-    }));
-
-    const mercury = statusArray.filter((data)=>{
-        return data.tool === "mercury"
-    })
-
-    console.log(mercury);
-
-    const url = `/raw/${sessionId}/${mercury[0].id}/${mercury[0].tool}`;
-    console.log(url);
-
-    if(mercury[0].status == "Complete"){
-      await fetch(url)
-        .then((response) => {
-          return response.json();
-        })
-        .then((jsonData) => {
-          var ipData = {
-            type:"ip",
-            data:jsonData[0],
-            // data:json,
-            firstKey:"dst_ip",
-            secondKey:"src_ip"
-          }
-    
-          setHeatmapData(ipData);
-    
-          var portData = {
-            type:"port",
-            data:jsonData[0],
-            // data: json,
-            firstKey:"dst_port",
-            secondKey:"src_port"
-          }
-    
-          setHeatmapData(portData);
-        });
-     }
-
-  }
-
-  handleCookies = (termsAccepted) => {
-=======
   clearResults = () =>{
->>>>>>> cf11b1644903bcb359730349b002cc06679b4c7e
     const { cookies } = this.props;
     cookies.remove(COOKIE_NAME)
     localStorage.clear(); 
     window.location.reload(false);
   }
 
-<<<<<<< HEAD
-  render() { 
-    const { ipResults, portResults } = this.state;
+  fetchStatsData = async () => {
+    const { setPacketStatisticsData, files, statuses } = this.props;
 
-=======
+    const tsharkObject = pcapStatsData[0]['tshark'];
+
+    setPacketStatisticsData(tsharkObject);
+  }
+  
   render() {
-    const refreshInterval = this.props.refreshInterval || 5;
->>>>>>> cf11b1644903bcb359730349b002cc06679b4c7e
+    const refreshInterval = this.props.refreshInterval 
+    let { ipResults, portResults, packetStats } = this.state; 
+    console.log(ipResults, portResults, packetStats) 
+
     return (
       <>
         <Navbar/>
@@ -159,7 +147,7 @@ class App extends React.Component {
           </Grid.Row>
           <Grid.Row columns={1}>
             <Grid.Column>
-              <DataMonitor sessionId={this.state.sessionId} files={this.props.rows} statuses={this.props.statuses} refreshInterval={refreshInterval}/>
+              <DataMonitor sessionId={this.state.sessionId} files={this.props.files} statuses={this.props.statuses} refreshInterval={refreshInterval}/>
             </Grid.Column>
           </Grid.Row>
           <Grid.Row columns={1}>
@@ -170,15 +158,14 @@ class App extends React.Component {
           {ipResults &&
           <Grid.Row >
             <Grid.Column >
-              <Heatmap key="1" data={ipResults.transformedData} keys={ipResults.uniqueListOfKeys} index="firstKey" name="Destination IP" width={800} height={500}/>
+              <Heatmap key="1" data={ipResults.data} keys={ipResults.keys} index="firstKey" name="Destination IP" width={800} height={500}/>
             </Grid.Column>
           </Grid.Row>
           }
-
           {portResults &&
           <Grid.Row >
             <Grid.Column >
-              <Heatmap key="1" data={portResults.transformedData} keys={portResults.uniqueListOfKeys} index="firstKey" name="Destination Port" width={800} height={1000}/>
+              <Heatmap key="1" data={portResults.data} keys={portResults.keys} index="firstKey" name="Destination Port" width={800} height={500}/>
             </Grid.Column>
           </Grid.Row>
           }
@@ -189,19 +176,15 @@ class App extends React.Component {
 }
 
 const mapStateToProps = (state, ownProps) => {
-<<<<<<< HEAD
-
   const results = getResults(state);
   const toolStatuses = getToolStatuses(state);
+  const toolResults = getToolResults(state);
   const wrangledData = getDataWranglingState(state);
-=======
-  const results = getResults(state)
-  const toolStatuses = getToolStatuses(state)
->>>>>>> cf11b1644903bcb359730349b002cc06679b4c7e
 
   return{
-    rows: results.rows || [],
+    files: results.rows || [],
     statuses: toolStatuses || {},
+    results: toolResults || {},
     vizData: wrangledData || {},
   }
 };
@@ -210,13 +193,10 @@ const mapDispatchToProps = {
     setSessionId,
     fetchResults,
     fetchToolStatus,
-<<<<<<< HEAD
-    setHeatmapData
-=======
     fetchTools,
     startFetchResults,
     stopFetchResults,
->>>>>>> cf11b1644903bcb359730349b002cc06679b4c7e
+    setPacketStatisticsData
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(withCookies(App));
