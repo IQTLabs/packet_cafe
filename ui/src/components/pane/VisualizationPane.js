4ec8ce15e6886ef8682fbebcbd6796ad74d7a651
Ryan <rashley@iqt.org>
2020-11-03 12:08:55 -0500

    linting fixes for pane and pcap components

--- ui/src/components/pane/VisualizationPane.js
+++ ui/src/components/pane/VisualizationPane.js
@@ -3,26 +3,12 @@ import { connect } from 'react-redux';
 
 import { Tab } from 'semantic-ui-react';
 
-import { startFetchResults, stopFetchResults } from "epics/auto-fetch-results-epic"
-import { fetchResults } from 'epics/fetch-results-epic'
-import { fetchToolStatus } from 'epics/fetch-status-epic'
 import { fetchTools } from 'epics/fetch-tools-epic'
-import { setPacketStatisticsData,  getDataWranglingState, configureHeatmapData } from 'domain/data_wrangling';
-import { getResults, getToolStatuses, getToolResults } from 'domain/data';
 
 import Table from 'components/table/Table';
 
 
 class VizualizationPane extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      ipResults:null,
-      portResults: null,
-      packetStats:null,
-    };
-  }
 
   componentDidMount(){
     this.props.fetchTools();
@@ -52,27 +38,10 @@ class VizualizationPane extends React.Component {
   }
 }
 
-const mapStateToProps = (state, ownProps) => {
-  const results = getResults(state);
-  const toolStatuses = getToolStatuses(state);
-  const toolResults = getToolResults(state);
-  const wrangledData = getDataWranglingState(state);
-
-  return{
-    files: results.rows || [],
-    statuses: toolStatuses || {},
-    results: toolResults || {},
-    vizData: wrangledData || {},
-  }
-};
+const mapStateToProps = null;
 
 const mapDispatchToProps = {
-    fetchResults,
-    fetchToolStatus,
     fetchTools,
-    startFetchResults,
-    stopFetchResults,
-    setPacketStatisticsData
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(VizualizationPane);
